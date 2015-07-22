dd08a2deab173011ea3ef6a7f02a7beb2c2e010d
andrewcrab <13761224431@163.com>
2015-07-22 02:46:38 +0000

    [api] Add help option

--- CLI/http-server.js
+++ CLI/http-server.js
@@ -4,5 +4,14 @@ var argv = require("minimist")(process.argv.slice(2));
 var colors = require("colors");
 var http = require("http");
 
-console.log("Hello world".red);
-
+//Print help on -h or --help
+if(argv.h || argv.help){
+    var help = [
+        "Usage: root-directory [options]",
+        " -p --port     Port to run static server on [8080]",
+        " -s --silent   Supress messages",
+        " -h --help     You are in the help documentation"
+    ].join('\n');
+    
+    console.log(help);
+}
