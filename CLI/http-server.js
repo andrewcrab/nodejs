c2d362b2ce6db46a395592c1e71a3664f67ce503
andrewcrab <13761224431@163.com>
2015-07-22 03:14:34 +0000

    [API] Basic static server, not tested yet.

--- CLI/http-server.js
+++ CLI/http-server.js
@@ -3,15 +3,38 @@
 var argv = require("minimist")(process.argv.slice(2));
 var colors = require("colors");
 var http = require("http");
+var nodeStatic = require("node-static");
 
 //Print help on -h or --help
 if(argv.h || argv.help){
     var help = [
-        "Usage: root-directory [options]",
-        " -p --port     Port to run static server on [8080]",
+        "Usage: http-server"+ " root-directory ".red +"[options]",
+        " -p            Port to run static server on [8080]",
         " -s --silent   Supress messages",
         " -h --help     You are in the help documentation"
     ].join('\n');
     
     console.log(help);
 }
+
+//Silent loging if -s
+var log = (argv.s && argv.silent) ? (function() {}) : console.log;
+
+// //Start server and render files
+var port = argv.p || 8080;
+var root = argv._[0] || ".";
+var file = new(nodeStatic.Server)(root,{AutoIndex: true, cache: 3600});
+log("Starting static seb server for ".yellow + root.cyan + " on port ".yellow + port.toString().cyan + ".");
+http.createServer(function(request, response){
+    request.addListener('end', function(request, response){
+        file.serve(request, response);
+    });
+}).listen(port);
+
+process.on('SIGINT', function(){
+    log("\nStatic web server stopped.".red);
+    process.exit();
+})
+
+
+
